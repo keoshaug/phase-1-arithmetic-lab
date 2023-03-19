eec47da31523f50936d8fb2bc67b38d8f2ea49df
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-03-19 16:15:18 -0400

    arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 16;
+let num4 =6;
+let multiply = num1*num2;
+let mod = num3 % num4;
+let max = Math.max(2,3,4,5,6,7,8,9,10,12,15,20);
+let random = Math.floor(Math.random() * 10) + 1;
+Math.isInteger(random);
